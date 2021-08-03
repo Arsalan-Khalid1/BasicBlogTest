5d41cddd89570ae1b9177d61694f8202b6898eef
Arsalan Khalid <arslankhalid889@gmail.com>
2021-08-02 22:40:59 -0700

    user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,35 +1,30 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
-const PostSchema = new mongoose.Schema(
+
+const UserSchema = new mongoose.Schema(
   {
-    title: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-
-    photo: {
-      type: String,
-      required: true,
-    },
-
     username: {
       type: String,
       required: true,
       unique: true,
     },
-
-    desc: {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
       type: String,
       required: true,
     },
-    categories: {
-      type: Array,
-      required: false,
+    profilePic: {
+      type: String,
+      default: "",
     },
   },
   { timestamps: true }
 );
 
-PostSchema.plugin(uniqueValidator);
-module.exports = mongoose.model("Post", PostSchema);
+UserSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", UserSchema);
